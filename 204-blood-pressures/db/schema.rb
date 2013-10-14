8ba6f0cb278f97a340d31efdc0103eff945c2aff
Kathleen Schuster <kschust@comcast.net>
2013-10-14 17:31:22 -0600

    Day 4: Exercise 3 in 204 mods - bp readings (dbase)

--- 204-blood-pressures/db/schema.rb
+++ 204-blood-pressures/db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131014221253) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "pressure_readings", force: true do |t|
+    t.string  "name"
+    t.integer "systolic"
+    t.integer "diastolic"
+  end
 
 end
--- 204-blood-pressures/db/seeds.rb
+++ 204-blood-pressures/db/seeds.rb
@@ -1,1 +1,14 @@
-# TODO: fill this in
+Pressure_reading.create!\
+  name: "Ed", 
+  systolic: 140 , 
+  diastolic: 90
+  
+Pressure_reading.create!\
+  name: "Jan", 
+  systolic: 110 , 
+  diastolic: 70
+    
+Pressure_reading.create!\
+  name: "Tim", 
+  systolic: 120 , 
+  diastolic: 80
