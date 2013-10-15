01045162f05915af445fe1d1ba7a2d5c4247500d
Kathleen Schuster <kschust@comcast.net>
2013-10-15 08:15:50 -0600

    Day 4: Exercise 5 in 207 - colored vehicle table (dbase)

--- 207-colorful-vehicles/db/seeds.rb
+++ 207-colorful-vehicles/db/seeds.rb
@@ -1,1 +1,8 @@
-# TODO: write this
+Color.create! name: "fuschia", html_color_code: "#FF00FF"  
+Color.create! name: "green", html_color_code: "#00FF00"
+Color.create! name: "purple", html_color_code: "#6666FF"
+Color.create! name: "yellow", html_color_code: "#FFFF00"  
+Vehicle.create! name: "airplane", image_filename: "plane.png"
+Vehicle.create! name: "bicycle", image_filename: "bike.png"
+Vehicle.create! name: "bus", image_filename: "bus.png"
+Vehicle.create! name: "car", image_filename: "car.png"
