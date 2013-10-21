15ebf4c5fe96aa8c25cf3268be3eeb04cb4f8a25
Kathleen Schuster <kschust@comcast.net>
2013-10-21 17:13:45 -0600

    Day 7: 218 - various exercises

--- 218-rake-console/controller.rb
+++ 218-rake-console/controller.rb
@@ -10,8 +10,26 @@ get "/elements/:id" do
   halt erb(:show_element)
 end
 
+get "/create_element" do
+  oxygen = Element.new
+  oxygen.name = "Oxygen"
+  oxygen.number = 16
+  oxygen..symbol = "O"
+  oxygen.save!
+  
+  halt erb(:index)
+end
+
 get "/cities/:id" do
   id = params[:id]
   @city = WorldCity.find(id)
   halt erb(:show_city)
+end
+
+get "/population_double" do
+  c = WorldCity.find(2)
+  c.population_in_millions *=2
+  c.save!
+  
+  halt erb(:index)
 end
