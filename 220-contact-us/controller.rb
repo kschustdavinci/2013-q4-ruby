2e4d43c4940dc52e516ef1baf2795bcbae7b1768
Kathleen Schuster <kschust@comcast.net>
2013-10-23 18:15:04 -0600

    Day 8: Exercise 3 & 4 in 220 (Post params)

--- 220-contact-us/controller.rb
+++ 220-contact-us/controller.rb
@@ -4,4 +4,10 @@ get "/" do
   halt erb(:index)
 end
 
-# TODO: write POST handler
+post "/" do
+  @message = Message.new 
+  @message.subject = params["subject"] 
+  @message.body = params["body"] 
+  @message.save! 
+  redirect "/"
+end
