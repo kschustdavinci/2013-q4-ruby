b2e0f6673d75e9fa61e059bb7a323b1b335edf22
Kathleen Schuster <kschust@comcast.net>
2013-10-31 18:02:56 -0600

    Day 11: Exercise 1 in 235 (stations: all,where,first,find_by)

--- 235-b-cycle-stations/controller.rb
+++ 235-b-cycle-stations/controller.rb
@@ -6,26 +6,29 @@ get "/" do
 end
 
 get "/existing" do
-  # TODO: Assign to @stations variable
+  @stations = Station.where(status: "existing")
   halt erb(:index)
 end
 
 get "/private" do
-  # TODO: Assign to @stations variable
+  @stations = Station.where(property_type: "private")
   halt erb(:index)
 end
 
 get "/zip/:zip_code" do
-  # TODO: Assign to @stations variable
+  zip = params["zip_code"]
+  @stations = Station.where(zip_code: zip)
   halt erb(:index)
 end
 
 get "/street/Broadway" do
-  # TODO: Assign to @stations variable
+  street = params["substring"]
+  @stations = Station.where("station_name like ?", street)
   halt erb(:index)
 end
 
 get "/street/17th" do
-  # TODO: Assign to @stations variable
+  street = params["substring"]
+  @stations = Station.where("station_name like ?", street)
   halt erb(:index)
 end
