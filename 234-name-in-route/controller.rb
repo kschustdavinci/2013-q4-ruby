b826f97f531beab9e39988919389dd105f36c422
Kathleen Schuster <kschust@comcast.net>
2013-10-31 18:04:25 -0600

    Day 11: Exercise 3 in 234 (birds: GET updates)

--- 234-name-in-route/controller.rb
+++ 234-name-in-route/controller.rb
@@ -5,6 +5,8 @@ get "/" do
   halt erb(:index)
 end
 
-# TODO: write GET handler to serve show.html.erb with @entry set
-# to the correct Encyclopedia Entry for routes like /albatross 
-# and /jay
+get "/:id" do
+  bird = params[:id]
+  @entry = EncyclopediaEntry.find_by(word: bird)
+  halt erb(:show)
+end
