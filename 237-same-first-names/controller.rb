bc5b10c37049d64119573e530a6bd0524b29c014
Kathleen Schuster <kschust@comcast.net>
2013-10-31 18:07:10 -0600

    Day 11: Exercise 6 in 237 (Users -- display different pages based on values entered)

--- 237-same-first-names/controller.rb
+++ 237-same-first-names/controller.rb
@@ -10,9 +10,26 @@ post "/" do
   #       their info.
   # TODO: If 2+ people have that first name, show choose.html.erb
   #       to let the user choose between the people with that first name.
+  
+  first_name = params["first_name"]
+  people = Person.where(first_name: first_name)
+  if people == Array.new
+    halt erb(:no_one)
+  elsif people.first == people.last
+    @person = Person.find_by(first_name: first_name)
+    halt erb(:number)
+  else
+    @people = people
+    halt erb(:choose)
+  end
 end
 
 get "/person/:first/:last" do
   # TODO: Show the info for the person with the first and last name
   #       specified in the route.
+  
+  first_name = params["first"]
+  last_name  = params["last"]
+  @person = Person.find_by(first_name: first_name, last_name: last_name)
+  halt erb(:number)
 end
