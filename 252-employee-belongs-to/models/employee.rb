ad25c476be82d6f5519f4d8e660df8f08a89470e
Kathleen Schuster <kschust@comcast.net>
2013-11-07 09:32:34 -0700

    Day 14: Exercise 252 (foreign-key lookup with belongs_to method)

--- 252-employee-belongs-to/models/employee.rb
+++ 252-employee-belongs-to/models/employee.rb
@@ -1,4 +1,5 @@
 class Employee < ActiveRecord::Base
+  belongs_to :company, class_name: "Company", foreign_key: "company_id"
   validates :first_name, presence: true
   validates :last_name, presence: true
 end
