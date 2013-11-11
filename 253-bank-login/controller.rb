37364ad7de6ec86aca4fab21a614ec3672f90d6a
Kathleen Schuster <kschust@comcast.net>
2013-11-11 16:57:39 -0700

    Day 16: Exercise 253 - (session variables, userid & password checking)

--- 253-bank-login/controller.rb
+++ 253-bank-login/controller.rb
@@ -16,29 +16,60 @@ post "/login" do
   # and either log them in, saving their user ID to the session,
   # and redirect to /accounts, or set an error message in @error
   # and re-render login.html.erb.
-
-  redirect "/accounts"
+  username = params["username"]
+  password = params["password"]
+  user = User.find_by(username: username)
+  
+  if user != nil
+    if 
+      user.password != password
+      @error = "Incorrect password"
+      halt erb(:login)
+    else
+        session[:user_id] = user.id
+        redirect "/accounts"
+    end
+  else
+    @error = "Username is unknown"
+    halt erb(:login)
+  end  
 end
 
 get "/accounts" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
+  if session[:user_id] == nil
+    redirect "/login"
+  else
+    @user = User.find(session[:user_id])
+    # TODO: Load the user based on the id saved to the session
 
-  halt erb(:accounts)
+    halt erb(:accounts)
+  end
 end
 
 get "/location" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
+  if session[:user_id] == nil
+    redirect "/login"
+  else
+    @user = User.find(session[:user_id])
+    # TODO: Load the user based on the id saved to the session
 
-  halt erb(:location)
+    halt erb(:location)
+  end
 end
 
 get "/rates" do
-  @user = User.find(1) # TODO: Remove this line
-  # TODO: Load the user based on the id saved to the session
+  if session[:user_id] == nil
+    redirect "/login"
+  else
+    @user = User.find(session[:user_id])
+    # TODO: Load the user based on the id saved to the session
 
-  halt erb(:rates)
+    halt erb(:rates)
+  end
 end
 
 # TODO: Write handler for GET /logout that logs out the user
+get "/logout" do
+  session.clear
+  redirect "/login"
+end
