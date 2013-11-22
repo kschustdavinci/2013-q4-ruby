298134989bdc207d04acfd4a4ba0ad7298ae2ce1
Kathleen Schuster <kschust@comcast.net>
2013-11-22 08:58:41 -0700

    Day 20: Exercise 303 - Rails sandwich orders

--- 303-sandwich-shop/app/controllers/main_controller.rb
+++ 303-sandwich-shop/app/controllers/main_controller.rb
@@ -7,7 +7,32 @@ class MainController < ApplicationController
 
   def place_order
     # TODO: Create a new SubOrder and render the thank you page if successful
-    raise "TODO: start by removing this line"
+    @order = SubOrder.new
+    @order.sandwich_type  = params["sandwich_type"]
+    @order.bread_type     = params["bread_type"]
+    @order.is_twelve_inch = params["is_twelve_inch"]
+    @order.has_extra_meat = params["has_extra_meat"]
+    @order.has_chips      = params["has_chips"]
+    @order.created_at     = curre
+    
+    @order.total_price = 3.49
+    
+    if @order.is_twelve_inch == true
+      @order.total_price = @order.total_price + 2.00
+    end
+    if @order.has_extra_meat == true
+      @order.total_price = @order.total_price + 0.49
+    end
+    if @order.has_chips == true
+      @order.total_price = @order.total_price + 0.99
+    end
+    
+    if @order.save
+      redirect_to "/thank_you" and return
+    else
+      render :order_form and return
+    end
+    
   end
 
   def list_orders
--- 303-sandwich-shop/config/routes.rb
+++ 303-sandwich-shop/config/routes.rb
@@ -2,4 +2,5 @@ App::Application.routes.draw do
   get  "/"            => "main#order_form"
   post "/"            => "main#place_order"
   get  "/list_orders" => "main#list_orders"
+  get  "/thank_you"   => "main#thank_you"
 end
