15b8296808a5b057255299303e5c2f03bd4d2598
Kathleen Schuster <kschust@comcast.net>
2013-11-23 18:20:58 -0700

    Day 21: Exercise 307 -- relationships: 1-to-many

--- 307-people-and-tools/db/seeds.rb
+++ 307-people-and-tools/db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Person.create! id: 1, name: "Eve"
+Person.create! id: 2, name: "Ed"
+Person.create! id: 3, name: "Tony"
+Tool.create! name: "bandsaw", person_id: 1
+Tool.create! name: "drill press", person_id: 1
+Tool.create! name: "lathe", person_id: 2
+Tool.create! name: "planer", person_id: nil
