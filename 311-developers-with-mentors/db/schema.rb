c9481e12d076324bb4b924483c2a04c145b06f63
Kathleen Schuster <kschust@comcast.net>
2013-11-23 18:31:03 -0700

    Day 21: Exercise 311 -- relationships: many-to-many with 1 table (2nd table is needed)

--- 311-developers-with-mentors/db/schema.rb
+++ 311-developers-with-mentors/db/schema.rb
@@ -11,9 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20131123012004) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "developers", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "mentorings", id: false, force: true do |t|
+    t.integer "mentor_developer_id"
+    t.integer "mentee_developer_id"
+  end
 
 end
--- 311-developers-with-mentors/db/seeds.rb
+++ 311-developers-with-mentors/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Developer.create! id: 1, name: "Ted"
+Developer.create! id: 2, name: "Ursula"
+Developer.create! id: 3, name: "Rita"
+Developer.create! id: 4, name: "Dan"
+Developer.create! id: 5, name: "Paula"
+Developer.create! id: 6, name: "Phil"
+Developer.find(2).mentee_ids = [3, 4]
+Developer.find(1).mentee_ids = [3]
+Developer.find(5).mentee_ids = [6]
+Developer.find(6).mentee_ids = [5]
