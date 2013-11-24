4572e527089c3c176400b7221c789d86a859575e
Kathleen Schuster <kschust@comcast.net>
2013-11-23 18:22:17 -0700

    Day 21: Exercise 309 -- relationships: many-to-many #2

--- 309-actors-and-plays/db/seeds.rb
+++ 309-actors-and-plays/db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Actor.create! id: 1, name: "Fred"
+Actor.create! id: 2, name: "Lynne"
+Actor.create! id: 3, name: "Cathy"
+Play.create!  id: 1, title: "Macbeth"
+Play.create!  id: 2, title: "Twelfth Night"
+Play.create!  id: 3, title: "Hamlet"
+Role.create!  role_name: "Macbeth", actor_id: 1, play_id: 1
+Role.create!  role_name: "Lady Macbeth", actor_id: 2, play_id: 1
+Role.create!  role_name: "Viola", actor_id: 2, play_id: 2
+Role.create!  role_name: "Hamlet", actor_id: nil, play_id: 3
