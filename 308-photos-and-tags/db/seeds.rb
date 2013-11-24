c35d91f649bc15d5c504a2f956636f6f177eb266
Kathleen Schuster <kschust@comcast.net>
2013-11-23 18:21:38 -0700

    Day 21: Exercise 308 -- relationships: many-to-many #1

--- 308-photos-and-tags/db/seeds.rb
+++ 308-photos-and-tags/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Photo.create! id: 1, name: "Flatirons at Dawn"
+Photo.create! id: 2, name: "Flatirons at Sunset"
+Photo.create! id: 3, name: "Sunset from My Window"
+Photo.create! id: 4, name: "Mysterious Blur"
+Tag.create! id:1, name: "Flatirons"
+Tag.create! id:2, name: "dawn"
+Tag.create! id:3, name: "sunset"
+Tag.create! id:4, name: "Pluto"
+Photo.find(1).tag_ids = [1,2]
+Photo.find(2).tag_ids = [1,3]
+Photo.find(3).tag_ids = [3]
